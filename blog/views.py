8bf9bdd791f75992d9062378fd5b6f1de296957e
laurahutton <laura.hutton802@gmail.com>
2016-04-18 00:54:50 +0000

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render 
 
-def index(request):
-    return HttpResponse("Hello, world. Welcome to the blog index.")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
